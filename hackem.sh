537c1f455929e212585bafc46d672d63cd3fc656
Isaac Lupin <95302944+GatorBash@users.noreply.github.com>
2022-06-28 09:13:19 -0500

    Create hackem.sh

---
+++ hackem.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "beginning hack....."
+sleep 1
+echo "connecting....."
+sleep 1
+echo "CONNECTED"
+sleep 1
+echo "hacking nasa..."
+sleep 2
+echo "10%"
+sleep 1
+echo "25%"
+sleep 1
+echo "31%"
+sleep 3
+echo "60%"
+sleep 1
+echo "70%"
+sleep 5
+echo "100%"
+sleep 1
+echo "NASA HACKED"
+echo "GIT GOOD LOSER"
